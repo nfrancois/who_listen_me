766895b1eb6ba0036d89745bc7f97090679cc263
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-08-15 20:13:08 +0200

    new test and refacto'

--- src/internal/google_json_cleaner.dart
+++ src/internal/google_json_cleaner.dart
@@ -2,35 +2,51 @@
 class _GoogleJsonCleaner {
   
   static final String STARTING_BLOCK = ")]}'\n\n";
+  static final String NULL_VALUE = 'null';
+  static final String COMMA = ',';
+  static final String OPEN_BRACE = '[';
+  static final String CLOSE_BRACE = ']';
+  static final String NEW_LINE = '\n';
+  static final String SPACE = ' ';
+  static final String ESCAPE = '\\';
+  static final String QUOTE = '"';
   
   String clean(String text){
     var isInStringValue = false;
-    var lastChar = null;
+    var lastChar = '';
     var buffer = new StringBuffer();
     // Don't need  first chars )]}'\n\n
-    var workingTest = (text.startsWith(STARTING_BLOCK)) ? text.substring(6) : text;
-    List<String> textChars = workingTest.splitChars();
-    for(String currentChar in textChars){
-      // Detect string value
-      // TODO escape
-      if(currentChar == '"'){
-        isInStringValue = !isInStringValue;
+    var workingTest = (text.startsWith(STARTING_BLOCK)) ? text.substring(STARTING_BLOCK.length) : text;
+    workingTest.splitChars().forEach((currentChar) {
+      if(currentChar != NEW_LINE){// Skip new line
+        // Detect string value
+        // TODO escape
+        //if(currentChar == QUOTE){
+        //  isInStringValue = !isInStringValue;
+        //}
+        // Add empty value
+        if(_betweenTwoComma(lastChar, currentChar) || _betweenCommaAndBrace(lastChar, currentChar) || _betweenBraceAndComma(lastChar, currentChar)){
+          buffer.add(NULL_VALUE);
+        }
+        
+        buffer.add(currentChar);
+        
+        lastChar = currentChar;
       }
-      // Skip whitespace but not in a string value
-      if(!isInStringValue && currentChar == '\n' ){
-        continue;
-      }
-      // Add empty value
-      // TODO cas ',]‘
-      if(!isInStringValue && currentChar == ',' && (lastChar == ',' || lastChar == '[')){
-        buffer.add("null");
-      }
-      
-      buffer.add(currentChar);
-      
-      lastChar = currentChar;
-    }
+    });
     return buffer.toString();
   }
+  
+  bool _betweenTwoComma(String firstChar, String secondChar){
+    return firstChar == COMMA && secondChar == COMMA;   
+  }
+  
+  bool _betweenCommaAndBrace(String firstChar, String secondChar){
+    return firstChar == COMMA && secondChar == CLOSE_BRACE;    
+  }
+  
+  bool _betweenBraceAndComma(String firstChar, String secondChar){
+    return firstChar == OPEN_BRACE && secondChar == COMMA;
+  }  
   
 }
--- test/internal/google_json_clean_test.dart
+++ test/internal/google_json_clean_test.dart
@@ -6,17 +6,12 @@ class GoogleJsonCleanerTest {
   _GoogleJsonCleaner _cleaner;
   //= const new _GoogleJsonCleaner();
   
-  /**
-   * A complete response.
-   */ 
   _completeResponse(){
       // Given
       String incoming = (new File("test/resources/incoming.txt")).readAsTextSync();
       String expected = (new File("test/resources/incoming_clean.txt")).readAsTextSync();  
-      
       // When
       String result = _cleaner.clean(incoming);
-      
       // Then
       //expect(result, isNotNull(), false);
       expect(result, equals(expected));
@@ -25,20 +20,48 @@ class GoogleJsonCleanerTest {
   _shouldANullBeetweenComma(){
     // Given
     String incoming = ',,,';
-    
     // When
     String result = _cleaner.clean(incoming);
     // Then
     expect(result, equals(',null,null,'));    
   }
   
+  _shouldAddNullValueBetweenBraceAnComma(){
+    // Given
+    String incoming = '[,';
+    // When
+    String result = _cleaner.clean(incoming);
+    // Then
+    expect(result, equals('[null,'));       
+  }
+  
+  _shouldAddNullValueBetweenCommaAndBrace(){
+    // Given
+    String incoming = ',]';
+    // When
+    String result = _cleaner.clean(incoming);
+    // Then
+    expect(result, equals(',null]'));       
+  }  
+  
+  _ignoreNewLine(){
+    // Given
+    String incoming = 'something\n';
+    // When
+    String result = _cleaner.clean(incoming);
+    // Then
+    expect(result, equals('something'));    
+  }
   
   
   runTest(){
     group('', () {
       setUp(() => _cleaner = new _GoogleJsonCleaner());
-      test('complete response test', () => _completeResponse());
       test('null value must be added between comma', () => _shouldANullBeetweenComma());
+      test('null value must be added between brace and comma', () => _shouldAddNullValueBetweenBraceAnComma());
+      test('null value must be added between comma and brace', () => _shouldAddNullValueBetweenCommaAndBrace());
+      test('ignore new line char', () => _ignoreNewLine());
+      test('complete response test', () => _completeResponse());
     });
   }
   
