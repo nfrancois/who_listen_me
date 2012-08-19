b6107283c5e3977808647788dfbfe20bcf282a6e
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-08-19 18:30:31 +0200

    use a exector for exection

--- src/api/circles_api.dart
+++ src/api/circles_api.dart
@@ -3,16 +3,29 @@
  */
 class CirclesApi {
   
+ final  _CircleRequestExecutor _executor;
+  
+  CirclesApi() : _executor = new _CircleRequestExecutor();
   
   /**
    * Find information about you add you in their circler. 
    */
   CirclesRequest whoCircleMe(String googlePlusId){
-    var request = new _CirclesRequest(googlePlusId);
+    var request = new _CirclesRequest(googlePlusId, _executor);
     request._execute();
     return request;
   }
     
+}
+
+class _CircleRequestExecutor {
+  
+  final HttpClient _httpClient;
+  final _GoogleJsonCleaner _googleJsonCleanerTest;
+  final _CirclerMapper _circlerMapper;
+  
+  _CircleRequestExecutor() : _httpClient = new HttpClient(), _googleJsonCleanerTest = new _GoogleJsonCleaner(), _circlerMapper = new _CirclerMapper();    
+  
 }
 
 class _CirclesRequest implements CirclesRequest {
@@ -23,9 +36,7 @@ class _CirclesRequest implements CirclesRequest {
   static final String _HTTPS_PROXY = "http://nfrproxy.appspot.com/proxy?url=";
   
   final String _googlePlusId;
-  final HttpClient _httpClient;
-  final _GoogleJsonCleaner _googleJsonCleanerTest;
-  final _CirclerMapper _circlerMapper;
+  final _CircleRequestExecutor _executor;
   
   var _onResponseCallback;
   var _onErrorCallback;
@@ -34,11 +45,11 @@ class _CirclesRequest implements CirclesRequest {
    * CirclesApi constructor.
    * _googlePlusId : Google Plus Id.
    */
-  _CirclesRequest(this._googlePlusId) : _httpClient = new HttpClient(), _googleJsonCleanerTest = new _GoogleJsonCleaner(), _circlerMapper = new _CirclerMapper();    
+  _CirclesRequest(this._googlePlusId, this._executor);  
 
   _execute(){  
     var url = "$_HTTPS_PROXY$_INCOMING_PERSON_URL".replaceFirst(_GOOGLE_PLUS_ID_TOKEN_URL, _googlePlusId);
-    var connexion = _httpClient.getUrl(new Uri.fromString(url));
+    var connexion = _executor._httpClient.getUrl(new Uri.fromString(url));
     connexion.onResponse = (httpResponse) => _responseHandler(httpResponse); 
     connexion.onError = (error)  {
       if(_onErrorCallback != null){
@@ -52,8 +63,8 @@ class _CirclesRequest implements CirclesRequest {
       var bytes = new List();
       InputStream input = httpResponse.inputStream;
       input.onClosed = () {
-        var jsonText = _googleJsonCleanerTest.clean(bytes.getRange(6, bytes.length-6));
-        var circlesResponse = _circlerMapper.map(jsonText);
+        var jsonText = _executor._googleJsonCleanerTest.clean(bytes.getRange(6, bytes.length-6));
+        var circlesResponse = _executor._circlerMapper.map(jsonText);
         if(_onResponseCallback != null){
           _onResponseCallback(circlesResponse);
         }
